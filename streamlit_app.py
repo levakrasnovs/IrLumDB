e22329d972023f88fa656df9357bb6f504e31ecc
levakrasnovs <info@colab.ws>
2024-10-15 16:00:42 +0300

    qy

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ df['L3'] = df['L3'].apply(lambda x: canonize_smiles(x))
 lum = df[['λlum,nm', 'QY', 'solvent']]
 lum = lum[~lum['QY'].isna()]
 lum['QY'] = lum['QY'].apply(lambda x: float(x.replace('<', '').replace(',','.')))
+lum = lum[lum['solvent'].apply(lambda x: x in ['CH2Cl2', 'CH3CN', 'toluene', 'CH3OH', 'THF'])]
 
 col1intro, col2intro = st.columns(2)
 col1intro.markdown("""
